d60d228802cf6e78aea9cd40584065ef0f78f2fa
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-16 08:52:08 +0500

    implemented ef core unitOfWork

--- TestableUnitOfWork/UnitOfWork.Implementation.EF/EFUnitOfWork.cs
+++ TestableUnitOfWork/UnitOfWork.Implementation.EF/EFUnitOfWork.cs
@@ -1,6 +1,76 @@
+using System;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using UnitOfWork.Interfaces;
+
 namespace UnitOfWork.Implementation.EF
 {
-    public class EfUnitOfWork
+    public class EfUnitOfWork : IUnitOfWork
     {
+        private readonly DbContext _context;
+        private readonly IDbConnection _connection;  
+        private IDbTransaction _transaction;
+
+        private bool _disposed = false;
+        public EfUnitOfWork(DbContext context)
+        {
+            _context = context;
+            _connection = _context.Database.GetDbConnection();
+        }
+
+        public IDbConnection Connection => _connection;
+        public IDbTransaction Transaction => _transaction;
+        
+        public void Begin()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(EfUnitOfWork));
+            _transaction = _context.Database.BeginTransaction().GetDbTransaction();
+        }
+
+        public void Commit()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(EfUnitOfWork));
+            _transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(EfUnitOfWork));
+            _transaction.Rollback();
+            
+        }
+        
+        private void ReleaseUnmanagedResources()
+        {
+            // TODO release unmanaged resources here
+        }
+        
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Connection?.Dispose();
+                Transaction?.Dispose();
+                _context.Dispose();
+            }
+            ReleaseUnmanagedResources();
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~EfUnitOfWork()
+        {
+            Dispose(false);
+        }
     }
 }
---
+++ TestableUnitOfWork/UnitOfWork.Implementation.EF/PaymentContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitOfWork.Implementation.EF
+{
+    public sealed class PaymentContext : DbContext
+    {
+        public DbSet<Payment> Users { get; set; }
+        
+        public PaymentContext()
+        {
+            Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=payments;Username=postgres;Password=faridun");
+        }
+    }
+}
