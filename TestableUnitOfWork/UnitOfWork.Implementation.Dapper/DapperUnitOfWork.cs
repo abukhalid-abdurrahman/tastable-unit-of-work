b2a5ab77b1808ce28d26cea60e63ff835da4ef87
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-16 08:28:09 +0500

    added unitOfWork interface and implemented unitOfWork via dapper

--- TestableUnitOfWork/UnitOfWork.Implementation.Dapper/DapperUnitOfWork.cs
+++ TestableUnitOfWork/UnitOfWork.Implementation.Dapper/DapperUnitOfWork.cs
@@ -1,6 +1,70 @@
+using System;
+using System.Data;
+using UnitOfWork.Interfaces;
+
 namespace UnitOfWork.Implementation.Dapper
 {
-    public class DapperUnitOfWork
+    public class DapperUnitOfWork : IUnitOfWork
     {
+        private readonly IDbConnection _connection;  
+        private IDbTransaction _transaction;
+
+        private bool _disposed = false;
+        public DapperUnitOfWork(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IDbConnection Connection => _connection;
+        public IDbTransaction Transaction => _transaction;
+        
+        public void Begin()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(DapperUnitOfWork));
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(DapperUnitOfWork));
+            _transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(DapperUnitOfWork));
+            _transaction.Rollback();
+        }
+
+        private void ReleaseUnmanagedResources()
+        {
+            // TODO release unmanaged resources here
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Connection?.Dispose();
+                Transaction?.Dispose();
+            }
+            ReleaseUnmanagedResources();
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~DapperUnitOfWork()
+        {
+            Dispose(false);
+        }
     }
 }
--- TestableUnitOfWork/UnitOfWork.Interfaces/IUnitOfWork.cs
+++ TestableUnitOfWork/UnitOfWork.Interfaces/IUnitOfWork.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Data;
+
 namespace UnitOfWork.Interfaces
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        IDbConnection Connection { get; }
+        IDbTransaction Transaction { get; }
+        public void Begin();
+        public void Commit();
+        public void Rollback();
     }
 }
